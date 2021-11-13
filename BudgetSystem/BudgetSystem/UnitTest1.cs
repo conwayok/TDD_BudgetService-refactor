25f6b398d256c5dfe56c549cab4a968d65fd6a02
Ezra <e1z2r3a4@gmail.com>
2021-11-13 16:21:55 +0800

    Init

---
+++ BudgetSystem/BudgetSystem/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace BudgetSystem
+{
+    public class BudgetServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
