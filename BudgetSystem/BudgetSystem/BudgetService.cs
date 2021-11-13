85e2ebb84e7fd9c996b656214be1247714b4f993
Joey Chen <jay.hatelove@gmail.com>
2021-11-13 22:38:47 +0800

    [refactor] inline variable: budgets

--- BudgetSystem/BudgetSystem/BudgetService.cs
+++ BudgetSystem/BudgetSystem/BudgetService.cs
@@ -19,11 +19,8 @@ namespace BudgetSystem
 
         public decimal Query(DateTime start, DateTime end)
         {
-
-            var budgets = _budgetRepo.GetAll();
-
             var period = new Period(start, end);
-            return budgets.Sum(budget => budget.OverlappingAmount(period));
+            return _budgetRepo.GetAll().Sum(budget => budget.OverlappingAmount(period));
         }
     }
 
